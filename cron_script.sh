4473d111dbdb59837ec270c431c0241a70027eb8
Joseph Stocks <jpstocks63@gmail.com>
2023-05-01 11:39:35 -0500

    cron script

---
+++ cron_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the path to the current script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Set log file paths
+STDOUT_LOG="$SCRIPT_DIR/output.log"
+STDERR_LOG="$SCRIPT_DIR/error.log"
+
+# Run the Python script and redirect output and errors to log files
+cd $SCRIPT_DIR && poetry run python birthday_checker.py > "$STDOUT_LOG" 2> "$STDERR_LOG"
+
+
