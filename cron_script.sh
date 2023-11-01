fe00a25a4652e4933ca42120c03d47d625cc4bd9
Joseph Stocks <jpstocks63@gmail.com>
2023-11-01 15:56:34 -0500

    Refactor cron_script.sh - combined log of stdout and error

--- cron_script.sh
+++ cron_script.sh
@@ -6,10 +6,7 @@ export PATH="$PATH:$HOME/.local/bin"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Set log file paths
-STDOUT_LOG="$SCRIPT_DIR/output.log"
-STDERR_LOG="$SCRIPT_DIR/error.log"
+COMBINED_STDOUT_ERROR_LOG="$SCRIPT_DIR/cron.log"
 
 # Run the Python script and redirect output and errors to log files
-cd $SCRIPT_DIR && poetry run python main.py > "$STDOUT_LOG" 2> "$STDERR_LOG"
-
-
+cd $SCRIPT_DIR && poetry run python main.py > "$COMBINED_STDOUT_ERROR_LOG" 2>&1
